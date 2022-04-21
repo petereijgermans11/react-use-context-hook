08531b1846aa346420cdb4b81611fbf4bf8e8a44
petereijgermans <p.eijgermans11@gmail.com>
2022-04-21 22:26:33 +0200

    added init project

---
+++ use-context-hook-init/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../src/components/Navigation/Navigation';
+import SaladMaker from '../src/components/SaladMaker/SaladMaker';
+
+
+function App() {
+    return (
+        <>
+            <Navigation/>
+            <SaladMaker/>
+        </>
+    );
+}
+
+export default App;
